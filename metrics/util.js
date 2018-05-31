bd2045d4ae7e536d5ebd68e77b588b4edb6714fe
Shanqing Tan <Shanqing.Tan@ibm.com>
2018-05-31 09:14:34 -0400

    Add a flag to disable the activationid

--- metrics/util.js
+++ metrics/util.js
@@ -1,6 +1,6 @@
 // Convert metric name to full Grafana metric label
 // e.g. label -> <namespace>.<action_id>.<activation>.label
-const label = label => {
+const label = (label, config) => {
   const ns = process.env['__OW_NAMESPACE'] || ''
   const id = process.env['__OW_ACTION_NAME'] || ''
   const name = id.substring(`/${ns}/`.length)
@@ -8,7 +8,12 @@ const label = label => {
 
   // replace '.' chars with '_' as '.' is grafana label separator
   const escape = str => str.replace(/\./g, '_')
-  return `${escape(ns)}.${escape(name)}.${actv}.${label}`
+
+  if (config.ignore_activation_ids === true) {
+    return `${escape(ns)}.${escape(name)}.${label}`
+  } else {
+    return `${escape(ns)}.${escape(name)}.${actv}.${label}`
+  }
 }
 
 const flatten = arr => arr.reduce((a, b) => a.concat(b), [])
